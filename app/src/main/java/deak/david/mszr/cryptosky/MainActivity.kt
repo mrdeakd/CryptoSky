8d4a11403d985136cf50322dca7cc63bcbb5579c
Deák Dávid <mrdeakd@gmail.com>
2021-03-23 22:01:36 +0100

    add navigator dependency to RainbowCake

--- app/src/main/java/deak/david/mszr/cryptosky/MainActivity.kt
+++ app/src/main/java/deak/david/mszr/cryptosky/MainActivity.kt
@@ -1,12 +1,14 @@
 package deak.david.mszr.cryptosky
 
-import androidx.appcompat.app.AppCompatActivity
+import co.zsmb.rainbowcake.navigation.SimpleNavActivity
 import android.os.Bundle
+import deak.david.mszr.cryptosky.ui.mainScreen.MainScreenFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : SimpleNavActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigator.add(MainScreenFragment())
     }
 }
