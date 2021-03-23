c9becd4a795ae8069a099333e3882f7733636395
Deák Dávid <mrdeakd@gmail.com>
2021-03-23 16:45:19 +0100

    aboutViewModel add to ViewModelModule to inject

--- app/src/main/java/deak/david/mszr/cryptosky/di/ViewModelModule.kt
+++ app/src/main/java/deak/david/mszr/cryptosky/di/ViewModelModule.kt
@@ -5,6 +5,7 @@ import co.zsmb.rainbowcake.dagger.ViewModelKey
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
+import deak.david.mszr.cryptosky.ui.aboutScreen.AboutScreenViewModel
 import deak.david.mszr.cryptosky.ui.detailsScreen.DetailsScreenViewModel
 import deak.david.mszr.cryptosky.ui.mainScreen.MainScreenViewModel
 
@@ -19,4 +20,9 @@ abstract class ViewModelModule {
     @IntoMap
     @ViewModelKey(DetailsScreenViewModel::class)
     abstract fun bindDetailsViewModel(detailsViewModel: DetailsScreenViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(AboutScreenViewModel::class)
+    abstract fun bindAboutViewModel(aboutViewModel: AboutScreenViewModel): ViewModel
 }
